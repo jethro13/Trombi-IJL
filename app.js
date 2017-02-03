629e14a9f93c1afaa12eded2e266bc4a3511cc3a
likhe <likhelifi@laposte.net>
2017-02-03 13:51:06 +0100

    orth error

---
+++ app.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+function addBackgroundImage(){
+
+}
+
+function drawHourHand(){
+
+}
+
+function drawMinuteHand(){
+
+}
+
+function drawSecondHand(){
+
+}
+
+function writeBrandName(){
+
+}
+
+function createClock(){
+
+}
+
+function clockApp(){
+
+}
+clockApp(); // Start the application
